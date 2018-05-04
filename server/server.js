448550f0352e96184b6fe0f8d4d635b8d5191b5e
yokeney <965660471@qq.com>
2018-05-04 17:44:57 +0800

    chartroom

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express=require('express')
+const mongoose=require('mongoose')
+//连接mongo使用imooc这个合集
+const DB_URL='mongodb://localhost:27017/imooc'
+mongoose.connect(DB_URL);
+mongoose.connection.on('connected',()=>{
+	console.log('mongo connect success');
+})
+//新建app
+const app=express()
+app.get('/',(req,res)=>{
+	res.send('<h1>hello,world</h1>')
+})
+app.get('/data',(req,res)=>{
+	res.json({name:'yokea',age:"12"})
+})
+app.listen(9093,()=>{
+	console.log("node app start is port 9093");
+})
