27da2d2febccaa118d7f496d5ca1444a16382109
yokeney <965660471@qq.com>
2018-05-07 11:25:25 +0800

    user

--- server/server.js
+++ server/server.js
@@ -6,17 +6,40 @@ mongoose.connect(DB_URL);
 mongoose.connection.on('connected',()=>{
 	console.log('mongo connect success');
 })
-const User=mongoose.model('use',new mongoose.Schema({
+//创建user表
+const User=mongoose.model('user',new mongoose.Schema({
 	user:{type:String,require:true},
 	age:{type:Number,require:true}
 }))
+//新增数据
+User.create({
+	user:'名',
+	age:"12"
+},(err,doc)=>{
+	if (!err) {
+		console.log(doc);
+	}
+	else{
+		console.log(err);
+	}
+})
+//删除
+// User.remove({age:10},(err,doc)=>{
+// 	console.log(doc);
+// })
+//更新
+User.update({'user':'名'},{'$set':{age:26}},(err,doc)=>{
+	console.log(doc);
+})
 //新建app
 const app=express()
 app.get('/',(req,res)=>{
 	res.send('<h1>hello,world</h1>')
 })
 app.get('/data',(req,res)=>{
-	res.json({name:'yokea',age:"12"})
+	User.find({},(err,doc)=>{
+		res.json(doc)
+	})
 })
 app.listen(9093,()=>{
 	console.log("node app start is port 9093");
--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import React from 'react'
+import {Button} from 'antd-mobile'
+import 'antd-mobile/dist/antd-mobile.css'
+ export default class App extends React.Component{
+    render(){
+        const a='hx'
+        return(
+            <div>
+                <h1>{a}</h1>
+                <Button type="primary">111</Button>
+            </div>
+        )
+    }
 }
-
-export default App;
